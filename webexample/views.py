f140d94b6393a1151bbe2c110e62d935d31629fa
Dmitry <1chem0dan2@gmail.com>
2018-09-14 17:58:38 +0900

    second commit

--- webexample/views.py
+++ webexample/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h3>Hellow World!!!</h3>")
+    return HttpResponse("<h3>Hellow World!</h3>")
 
 # Create your views here.
